bb57ebec9a05583bcf080a75ae12b5f48bfc671d
CBraeuninger <carolin.braeuninger@gmail.com>
2020-04-14 21:48:06 +0200

    CalculateRMSE method implemented

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,37 +11,33 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+   VectorXd rmse(4);
+   rmse << 0,0,0,0;
+
+   // check the validity of the following inputs:
+   //  * the estimation vector size should not be zero
+   //  * the estimation vector size should equal ground truth vector size
+   if (estimations.size() != ground_truth.size()|| estimations.size() == 0) {
+      return rmse;
+   }
+
+   // accumulate squared residuals
+   for (unsigned int i=0; i < estimations.size(); ++i) {
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+
+      // coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+   }
+
+   // calculate the mean
+   rmse = rmse/estimations.size();
+
+   // calculate the squared root
+   rmse = rmse.array().sqrt();
+
+   // return the result
+   return rmse;
 }
 
-// MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-//   /**
-//    * Calculates the Jacobian of input state x_state (for radar measurement)
-//    */
-
-//     MatrixXd Hj(3,4);
-//    // recover state parameters
-//    float px = x_state(0);
-//    float py = x_state(1);
-//    float vx = x_state(2);
-//    float vy = x_state(3);
- 
-//    // Calculate rho
-//    float rho = sqrt(pow(px,2)+pow(py,2));
-
-//    if (rho==0)  // check division by zero => throws error
-//    {
-//       throw rho;
-//    }
-//    else // compute the Jacobian matrix
-//    {
-//       Hj << px / rho,                        py / rho,                        0,          0,
-//             -py / pow(rho,2),                px / pow(rho,2),                 0,          0,
-//             py * (vx*py-vy*px) / pow(rho,3), px * (vy*px-vx*py) / pow(rho,3), px / rho,   py / rho;
-
-//    }
-  
-//   return Hj;
-// }
